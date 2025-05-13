7e26875a54fe800584e0f8d1562d28119a569ecd
dudabones <galvaovsco@gmail.com>
2025-05-13 09:33:33 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -1,13 +1,3 @@
-/**
- * Exercício +PraTi Formação Desenvolvedor Full Stack Júnior 
- * 
- * Instruções para executar
- * 1. Instale o Node.js (https://nodejs.org)
- * 2. Salve este arquivo como exercicios.js
- * 3. Abra o terminal ou prompt de comando
- * 4. Navegue até a pasta onde salvou o arquivo: cd caminho/da/pasta
- * 5. Execute com o comando: node exercicios.js
- */
 // ---- EXERCÍCIO 1: Verificar se um número é par ou ímpar ----
 function verificarParImpar(numero) {
     if (numero % 2 === 0) {
